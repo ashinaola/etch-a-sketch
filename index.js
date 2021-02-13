496b97635fb2dc057533541611567540423cf337
ashinaola <shinaolaagbede728@gmail.com>
2021-02-12 21:49:43 -0800

    Need to work on added 256 x 256

--- index.js
+++ index.js
@@ -9,15 +9,29 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(16, 16);
+function color() {
+  document.querySelectorAll('div .grid-item').forEach(grid => grid.addEventListener("mouseover", function(e) {
+    this.style.backgroundColor="Black"
+  }));
+}
 
-document.querySelectorAll('div .grid-item').forEach(grid => grid.addEventListener("mouseover", function(e) {
-  this.style.backgroundColor="Black"
-}));
+makeRows(16, 16);
+color();
 
 document.querySelector('#clear').addEventListener("click", function() {
   document.querySelectorAll('div .grid-item').forEach(grid2 => grid2.style.backgroundColor = "White");
 });
 
+document.querySelector('#big').addEventListener("click", function() {
+  document.querySelector("#container").innerHTML = "";
+  makeRows(256,256);
+  color();
+});
+
+document.querySelector('#small').addEventListener("click", function() {
+  document.querySelector("#container").innerHTML = "";
+  makeRows(16, 16);
+  color();
+});
 
 
